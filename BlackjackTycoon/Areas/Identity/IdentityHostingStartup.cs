de33999e36ed7856d6f2c4199690b9141ff7bef3
Joel Lyons <joel.lyons@outlook.com>
2020-03-04 17:26:05 -0600

    Configured Identity to use 'Username' instead of 'Email' when registering an account and logging in. I kept the 'Email' field when registering because Identity can use it for password resets.

---
+++ BlackjackTycoon/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using BlackjackTycoon.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BlackjackTycoon.Areas.Identity.IdentityHostingStartup))]
+namespace BlackjackTycoon.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
