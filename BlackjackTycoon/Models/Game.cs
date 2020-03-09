20204ab48c03bb270327595de0a4933c9784365b
cj633931 <cj633931@southeast.edu>
2020-03-09 14:49:41 -0500

    Created base for Game class as well as finished Coin class.

--- BlackjackTycoon/Models/Game.cs
+++ BlackjackTycoon/Models/Game.cs
@@ -7,5 +7,8 @@ namespace BlackjackTycoon.Models
 {
     public class Game
     {
+        private string Name { get; set; }
+        private List<string> Rules { get; set; }
+        private string Type { get; set; }
     }
 }
