7bbec4ba029c26e60867ea4793fa1389567b58bd
Jeff Barnett <jb187912@LR00108>
2020-03-09 14:50:24 -0500

    Created the Coin flip class TODO need to create db script yer

--- BlackjackTycoon/Models/CoinflipGame.cs
+++ BlackjackTycoon/Models/CoinflipGame.cs
@@ -5,7 +5,14 @@ using System.Threading.Tasks;
 
 namespace BlackjackTycoon.Models
 {
-    public class CoinflipGame
+    public class CoinflipGame:Game
     {
+        public int ID { get; set; }    
+        public decimal betAmount { get; set; }
+        private Player player { get; set; }
+        public decimal payOut { get; set; }
+        private bool result { get; set; }
+
+        
     }
 }
