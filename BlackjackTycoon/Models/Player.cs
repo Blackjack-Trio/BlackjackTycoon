3f4e2e33640008f2040dbcf6d0c46cee8926cab5
cj633931 <cj633931@southeast.edu>
2020-03-11 14:58:51 -0500

    Working on Player and GameSetup

--- BlackjackTycoon/Models/Player.cs
+++ BlackjackTycoon/Models/Player.cs
@@ -7,5 +7,13 @@ namespace BlackjackTycoon.Models
 {
     public class Player
     {
+        public ApplicationUser User { get; set; }
+        public decimal Pocket { get; set; }
+
+        public Player(ApplicationUser user, decimal pocket)
+        {
+            User = user;
+            Pocket = pocket;
+        }
     }
 }
