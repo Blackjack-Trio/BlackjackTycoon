ceb83cfc7b99ebdde22df5b1813c3057f33486cc
Chase Jenkins <cj633931@southeast.edu>
2020-03-20 03:31:16 -0500

    Cleaned up Coin model. Added string overrides.

--- BlackjackTycoon/Models/Coin.cs
+++ BlackjackTycoon/Models/Coin.cs
@@ -7,18 +7,37 @@ namespace BlackjackTycoon.Models
 {
     public class Coin
     {
+        private static readonly Random rnd = new Random();
+
         public bool Face { get; set; }
         
-        public Coin()
+        public Coin(string face="heads")
         {
-
+            if (face.Equals("heads"))
+            {
+                Face = true;
+            } else
+            {
+                Face = false;
+            }
         }
 
         public bool Flip()
         {
-            bool result = Convert.ToBoolean(new Random().Next(0, 1));
+            bool result = Convert.ToBoolean(rnd.Next(0, 1));
             Face = result;
             return result;
+        }
+
+        public override string ToString()
+        {
+            if (Face)
+            {
+                return "Heads";
+            } else
+            {
+                return "Tails";
+            }
         }
     }
 }
