2c2185048b0bebe18ecbc4e38ca6d1b73679b4b7
Joel Lyons <joel.lyons@outlook.com>
2020-04-08 18:14:50 -0500

    Created coinflip game logic in GameController. Updated GameController and GameSetup page to reflect controller changes. Updated Coinflip game view. Created Play() and Payout() methods in CoinflipGame.

--- BlackjackTycoon/Models/CoinflipGame.cs
+++ BlackjackTycoon/Models/CoinflipGame.cs
@@ -15,6 +15,30 @@ namespace BlackjackTycoon.Models
             Type = "Coin";
         }
 
-        
+        public bool Play(string selection, int bet)
+        {
+            /* variables */
+            bool result = false;
+            /* Generate either 0 or 1 randomly */
+            Random random = new Random();
+            var n = random.Next(0, 2);
+
+            /* take the bet amount from the players bankroll */
+            this.Player.Bankroll -= bet;
+  
+            /* Check if player won */
+            if (selection == "heads" && n == 1 || selection == "tails" && n == 0)
+            {
+                result = true;
+                Payout(bet);
+            }
+
+            return result;
+        }
+
+        private void Payout(int bet)
+        {
+            this.Player.Bankroll += bet * 2;
+        }
     }
 }
