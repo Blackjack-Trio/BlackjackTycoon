7c7e550700deb9383d8f5119c7b45d36479e5268
Joel Lyons <joel.lyons@outlook.com>
2020-04-15 17:28:16 -0500

    Began working on blackjack game logic.

--- BlackjackTycoon/Models/BlackjackGame.cs
+++ BlackjackTycoon/Models/BlackjackGame.cs
@@ -5,14 +5,45 @@ using System.Threading.Tasks;
 
 namespace BlackjackTycoon.Models
 {
-    public class BlackjackGame:Game
+    public class BlackjackGame : Game
     {
+        // props
+        private BlackjackDealer dealer;
+        private BlackjackPlayer player;
+        private int min_bet;
+        private int max_bet;
+        private int deck_count;
 
+        // constructors
         public BlackjackGame()
         {
             Name = "Blackjack";
             Rules.Add("Get 21 to win!");
             Type = "Card";
+        }
+
+        public BlackjackGame(int min_bet, int max_bet, int deck_count)
+        {
+            this.min_bet = min_bet;
+            this.max_bet = max_bet;
+            this.deck_count = deck_count;
+
+            try
+            {
+                this.dealer = new BlackjackDealer();
+                this.dealer.JoinGame(this);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception('could not create or add dealer to game: ' + ex);
+            }
+
+        }
+
+        // class methods
+        internal void Play()
+        {
+            throw new NotImplementedException();
         }
     }
 }
---
+++ BlackjackTycoon/Models/BlackjackPlayer.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlackjackTycoon.Models
+{
+    public class BlackjackPlayer
+    {
+        // props
+        private ApplicationUser user;
+        private BlackjackHand hand;
+        private Game game;
+
+        // constructors
+        public BlackjackPlayer() { }
+
+        public BlackjackPlayer(ApplicationUser user)
+        {
+            this.user = user;
+        }
+
+        // class methods
+        public BlackjackHand getHand()
+        {
+            return this.hand;
+        }
+
+        public void JoinGame(Game game)
+        {
+            if (this.game == null)
+            {
+                this.game = game;
+            }
+            else
+            {
+                throw new Exception("Player cannot join a new game before leaving previous game.");
+            }
+        }
+
+        public void LeaveGame()
+        {
+            this.game = null;
+        }
+
+        public void Bet(int amount)
+        {
+            if (game == null) throw new Exception("Player cannot bet if they are not part of a game.");
+            if (this.user.Bankroll < amount) throw new Exception("Player cannot bet more than they have.");
+
+            // take bet amount from players bankroll.
+            this.user.Bankroll -= amount;
+        }
+
+        public bool ShouldStandWith(BlackjackHand hand)
+        {
+            bool should = false;
+
+            if (hand.getValue() >= 16)
+            {
+                should = true;
+            }
+
+            return should;
+        }
+    }
+}
