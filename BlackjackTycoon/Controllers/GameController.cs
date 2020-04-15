4854269c9485929d1a9becde4eb48e5f78b8fa39
Joel Lyons <joel.lyons@outlook.com>
2020-04-15 16:19:01 -0500

    Made minor changes to coinflip game logic.

--- BlackjackTycoon/Controllers/GameController.cs
+++ BlackjackTycoon/Controllers/GameController.cs
@@ -19,44 +19,61 @@ namespace BlackjackTycoon.Controllers
             _userManager = userManager;
         }
 
-        public async Task<IActionResult> Coinflip(string coin, string bet, bool play = false)
+        [HttpPost]
+        public async Task<IActionResult> PlayCoinflipAsync(string selection, string bet)
         {
-            /* Errors array */
-            ViewBag.Errors = new List<string>();
-
             /* Get the current logged in user. */
             ViewBag.userId = _userManager.GetUserId(HttpContext.User);
             ApplicationUser user = _userManager.FindByIdAsync(ViewBag.userId).Result;
             ViewBag.User = user;
 
-            /* If play = true validate inputs + run game logic */
-            if (play)
+            /* validation */
+            ViewBag.Errors = new List<string>();
+
+            // checks the user selected heads or tails
+            if (selection == null) ViewBag.Errors.Add("Please select heads or tails.");
+
+            // checks if bet is an integer
+            if (!int.TryParse(bet, out int i)) ViewBag.Errors.Add("The bet must be an integer.");
+
+            // make sure the user has enough money to cover the bet
+            if (i > user.Bankroll) ViewBag.Errors.Add("You don't have enough money to place that bet.");
+
+            // bet must be greater than 0
+            if (i < 0) ViewBag.Errors.Add("You must bet more than $0");
+
+            /* If any errors got added to error list we'll return the view now */
+            if (ViewBag.Errors.Count >= 1) return View("Coinflip");
+
+            // play coinflip
+            CoinflipGame coinflip = new CoinflipGame();
+            coinflip.Player = user;
+
+            if (coinflip.Play(selection, i)) // i = players bet (i comes from the int.TryParse() method used in validation).
             {
-                /* validation */
-                if (coin == null) ViewBag.Errors.Add("Please select heads or tails.");
-                if (!int.TryParse(bet, out int i)) ViewBag.Errors.Add("The bet must be an integer.");
-                if (i > user.Bankroll) ViewBag.Errors.Add("You don't have enough money to place that bet.");
-                /* If any errors got added we'll return the view now */
-                if (ViewBag.Errors.Count >= 1) return View();
-
-                /* game logic */
-                CoinflipGame coinflip = new CoinflipGame();
-                coinflip.Player = user;
-
-                if (coinflip.Play(coin, int.Parse(bet)))
-                {
-                    ViewBag.GameResults = "Winner! You won $" + (int.Parse(bet)*2);
-                    ViewBag.LastGame = true;
-                }
-                else
-                {
-                    ViewBag.GameResults = "Loser! You lost $" + int.Parse(bet);
-                    ViewBag.LastGame = false;
-                }
-
-                IdentityResult result = await _userManager.UpdateAsync(user);
+                ViewBag.GameResults = "Winner! You won $" + (int.Parse(bet) * 2);
+                ViewBag.LastGame = true;
             }
-            
+            else
+            {
+                ViewBag.GameResults = "Loser! You lost $" + int.Parse(bet);
+                ViewBag.LastGame = false;
+            }
+
+            // update database
+            IdentityResult result = await _userManager.UpdateAsync(user);
+
+            // send them back to coinflip game view
+            return View("Coinflip");
+        }
+
+        public IActionResult Coinflip()
+        {
+            /* Get the current logged in user. */
+            ViewBag.userId = _userManager.GetUserId(HttpContext.User);
+            ApplicationUser user = _userManager.FindByIdAsync(ViewBag.userId).Result;
+            ViewBag.User = user;
+
             return View();
         }
 
--- BlackjackTycoon/Models/CoinflipGame.cs
+++ BlackjackTycoon/Models/CoinflipGame.cs
@@ -16,16 +16,16 @@ namespace BlackjackTycoon.Models
         }
 
         public bool Play(string selection, int bet)
-        {
-            /* variables */
+        {  
             bool result = false;
+
             /* Generate either 0 or 1 randomly */
             Random random = new Random();
             var n = random.Next(0, 2);
 
             /* take the bet amount from the players bankroll */
             this.Player.Bankroll -= bet;
-  
+
             /* Check if player won */
             if (selection == "heads" && n == 1 || selection == "tails" && n == 0)
             {
